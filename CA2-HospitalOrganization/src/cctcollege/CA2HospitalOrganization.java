c20f7943c22562d87837c2c20f486ffdedf88a9d
Student2024606CCT <2024606@student.cct.ie>
2025-04-26 02:07:04 +0100

    First commit
    Netbeans project created

---
+++ CA2-HospitalOrganization/src/cctcollege/CA2HospitalOrganization.java
@@ -0,0 +1,16 @@
+package cctcollege;
+
+/**
+ *
+ * @author asafeds
+ */
+public class CA2HospitalOrganization {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
